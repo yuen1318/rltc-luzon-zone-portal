4fa899ba7af21b786fd9683ab19c70390eff3d3d
yuen yalung <yuen.yalung@gmail.com>
2026-05-04 14:52:38 +0800

    push

---
+++ attendees.js
@@ -0,0 +1,8 @@
+const CSV_DATA = 
+`Name,Gender,Branch,Room Number,Bus Number,T1,T2,T3,T4,T5
+Yuen Y Yalung,M,Main Office,201,Bus 3,Web Development,AI & ML,,,
+Jose Lorenzo Cunanan,M,North Branch,305,Bus 1,Cloud Computing,DevOps,,,
+Exequil Saylon,M,South Branch,412,Bus 2,Data Science,Cybersecurity,,,
+Charlene G Yalung,F,Main Office,208,Bus 1,UI/UX Design,Web Development,,,
+Kathy Rose Santos,F,East Branch,315,Bus 3,AI & ML,Data Science,,,
+Princess Sarah Masabio,F,North Branch,502,Bus 2,Cloud Computing,Cybersecurity,,,`;
